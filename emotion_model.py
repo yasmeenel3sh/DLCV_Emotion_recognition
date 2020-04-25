a2ba0561d507eaf7bcf9dee775612d475477954a
Michael Khalil <mikegeorge2012@gmail.com>
2020-04-25 22:22:12 +0200

    add validation

--- emotion_model.py
+++ emotion_model.py
@@ -7,12 +7,14 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
 
+from sklearn.model_selection import train_test_split
+
 # from keras.optimizers import Adam
 # from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
 num_classes = 3
 rows, cols = 48, 48
-batch_size = 12
+batch_size = 32
 
 train_data = []
 labels = []
@@ -26,17 +28,19 @@ for i, emotion in enumerate(['anger','sadness', 'happy']):
     labels = np.append(labels, np.ones(x.shape[0]) * i)
 train_data = train_data.reshape(train_data.shape[0],rows,cols,1)
 
+x_train, x_valid, y_train, y_valid = train_test_split(train_data, labels, test_size=0.33, shuffle= True)
+
 model = Sequential()
 
 # Block 1
-model.add(Conv2D(1, (3,3), padding='same', kernel_initializer='he_normal', input_shape=(rows,cols,1)))
+model.add(Conv2D(10, (3,3), padding='same', kernel_initializer='he_normal', input_shape=(rows,cols,1)))
 model.add(Activation('elu'))
-# model.add(BatchNormalization())
-model.add(Conv2D(1, (3,3), padding='same', kernel_initializer='he_normal', input_shape=(rows,cols,1)))
+model.add(BatchNormalization())
+model.add(Conv2D(10, (3,3), padding='same', kernel_initializer='he_normal', input_shape=(rows,cols,1)))
 model.add(Activation('elu'))
-# model.add(BatchNormalization())
+model.add(BatchNormalization())
 model.add(MaxPooling2D((2,2),2))
-# model.add(Dropout(0.2))
+model.add(Dropout(0.2))
 
 # Block 5
 model.add(Flatten())
@@ -50,13 +54,19 @@ model.add(Activation('softmax'))
 
 print(model.summary())
 
-epochs = 3
+epochs = 30
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-one_hot_labels = keras.utils.to_categorical(labels, num_classes=num_classes)
+train_one_hot_labels = keras.utils.to_categorical(y_train, num_classes=num_classes)
+valid_one_hot_labels = keras.utils.to_categorical(y_valid, num_classes=num_classes)
 
-history = model.fit(train_data, one_hot_labels, epochs=epochs, batch_size=batch_size)
+history = model.fit(x_train, train_one_hot_labels, epochs=epochs, batch_size=batch_size, validation_data=(x_valid,valid_one_hot_labels))
 
 test = np.array(Image.open('2.jpg'))
-m = model.predict(test.reshape(1,48,48,1))
+test_set = []
+test_set.append(test)
+test_set.append(test)
+test_set = np.array(test_set)
+m = model.predict(test_set.reshape(test_set.shape[0],48,48,1))
 print(m)
+print(np.where(m==np.max(m)))
